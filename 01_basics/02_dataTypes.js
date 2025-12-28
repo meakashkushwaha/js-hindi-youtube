09a8267664d847e3e26c5d9ef017ec6dcc75d1da
Akash Kushwaha <mekushwahaakash28@gmail.com>
2025-12-28 19:51:20 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+console.log("Akash")
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Akash Kushwaha");
+
+let name = "Akash"
+let age = 22
+let isLoggedIn = false
+let state;
+// PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigint for very very long number using
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// Object 
+
+console.log(typeof undefined);// undefined 
+console.log(typeof null);// object
+
+
+
+
